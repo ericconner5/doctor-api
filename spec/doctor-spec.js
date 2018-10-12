efa4c99d8ff1c6670fe237787d9f4a30164409eb
Eric Conner <theluvq@gmail.com>
2018-10-12 16:22:22 -0700

    try some different ways to get test 1 to actually test the js

--- spec/doctor-spec.js
+++ spec/doctor-spec.js
@@ -3,24 +3,24 @@ import Search from './../src/index';
 describe('Search', function() {
 
   let userSearch;
-  console.log("here" + userSearch);
 
   beforeEach(function() {
     userSearch = new Search("37.773%2C-122.413", "fever");
 
+    console.log("here " + userSearch);
   });
 
   it('should test that an api query returns a list of portland doctors', function() {
-    const actual = userSearch.city;
-    const control = "37.773%2C-122.413";
-    expect(actual).toEqual(control);
+    console.log(Search);
+    const userSearch = new Search ('city');
+    expect(userSearch instanceof Search).toBe(true);
   });
-
-  it('should test that an empty api query returns an error message', function() {
-    const actual = userSearch.city;
-    const control = "";
-    expect(actual).toEqual(control);
-  });
+  // 
+  // it('should test that an empty api condition query returns an error message', function() {
+  //   const actual = userSearch.city;
+  //   const control = "";
+  //   expect(actual).toEqual(control);
+  // });
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+debugger
 
 export default class Search {
   constructor(city, condition){
@@ -22,27 +23,43 @@ export default class Search {
       "headers": {
         "cache-control": "no-cache",
         "Postman-Token": "a3ba4709-526c-49f7-9d72-32a3fc4da55f"
-      }
+      },
+      data: {
+        format: 'json'
+      },
+      success: (response) => {
+        const city = this.city;
+        const condition = this.condition;
+      },
+      error: function() {
+        console.log('error after api search');
+      },
+
     }
 
-    // $.ajax(settings).done(function() {
+    //
+    // $.ajax(Search).done(function() {
     //   // console.log(response);
     // });
 
-    // getKelvin() {
+    // getSearch() {
     //   $.ajax({
-    //     url: `http://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${process.env.WEATHER_API_KEY}`,
+    //     url: `https://api.betterdoctor.com/2016-03-01/doctors?query=fever&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.apiKey}`,
     //     type: 'GET',
     //     data: {
     //       format: 'json'
     //     },
     //     success: (response) => {
-    //       const temp = response.main.temp;
+    //       const city = this.city;
+    //       const condition = this.condition;
     //       this.kelvin = temp;
     //     },
     //     error: function() {
     //     },
     //   });
     // }
+
+
+
   }
 }
